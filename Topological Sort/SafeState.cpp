295d21f483d3716c8cb73f2a05f6d6e9fae2055f
Archana Chandrasekar <archanachandrasekar@192.168.1.8>
2023-12-17 08:23:26 +0530

    Change

--- Topological Sort/SafeState.cpp
+++ Topological Sort/SafeState.cpp
@@ -1,13 +1,14 @@
 //Question Link
-// https://leetcode.com/problems/find-eventual-safe-states/?envType=list&envId=pnz711or
-
+//https://leetcode.com/problems/find-eventual-safe-states/?envType=list&envId=pnz711or
+//Solution
 
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
+        //Construct a graph that leads from destination to source
         unordered_map<int,vector<int>>mp;
-        unordered_map<int,int>outdegree;
+        unordered_map<int,int>outdegree;  // the number of outgoing edges for each node
         queue<int>q;
 
         for(int i = 0;i<n;i++){
@@ -15,11 +16,14 @@ public:
                 mp[graph[i][j]].push_back(i);
             }
             outdegree[i] = graph[i].size();
+            // When we dont have any outgoing edges, then it is in a safe state
             if(outdegree[i] == 0){
                 q.push(i);
             }
         }
         vector<int>res;
+
+        // Keeping track of all the safe nodes.
         vector<int>safe(n,0);
 
         while(!q.empty()){
@@ -29,6 +33,7 @@ public:
 
             for(auto x:mp[t]){
                 outdegree[x]--;
+                // Again if all edges are explored , hence they are all coming from the safe nodes, hence current node is also safe.
                 if(outdegree[x] == 0){
                     q.push(x);
                 }
